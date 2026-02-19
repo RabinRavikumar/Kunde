4f91a2b11cdad028f82bc32ab8bb6f5ad63e789d
RabinRavikumar <rabin.ravikumar@outlook.de>
2026-02-20 00:07:14 +0100

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,3 +1,8 @@
+/**
+Die Klasse Queue implementiert eine Warteschlange:
+Elemente werden am Ende der Queue eingefügt (enqueue)
+und am Anfang der Queue entfernt (dequeue).
+**/
 public class Queue{
   private Element first;
   private Element last;
@@ -6,12 +11,14 @@ public class Queue{
     first = null;
     last = null
     }
+  //Gibt den  ersten Wert des Elements der Queue zurück.
   public Object front(){
     if (isEmpty()) {
       return null;
     } // end of if
     return first.getValue();
     }
+  //Fügt ein Element in die Queue ein.
   public void enqueue(Object pObject){
     if (pObject!=null) {
       Element element=new Element(pObject,null);
@@ -22,7 +29,8 @@ public class Queue{
       } // end of if-else
       last=element;
     }
-    }  
+    }
+  // löscht das Erste Element in der Queue.
   public void dequeue(){
     if (!isEmpty()) {
       first=first.getNext();
@@ -30,8 +38,10 @@ public class Queue{
         last=first;
       } // end of if
     }
-  } 
+  }
+  //Prüft ob die Queue leer ist.
   public boolean isEmpty(){
     return first==null;
     }        
   }
+
